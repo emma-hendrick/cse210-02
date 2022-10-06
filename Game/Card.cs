95ff0e42ecfd2146c7043810af04be63ada88a7c
Michael Hendrick <mhendrick@byui.edu>
2022-10-06 11:34:35 -0600

    Suits added to display

--- Game/Card.cs
+++ Game/Card.cs
@@ -46,18 +46,107 @@ namespace Hilo.Game
             string suitColor = (suit <= 1) ? blackOnWhite: redOnWhite;
 
             // Suit and number
-            string suitAndNumber = suitColor + suits[suit] + blackOnWhite + number;
+            string suitString = suitColor + suits[suit] + blackOnWhite;
+            string suitAndNumber = suitString + number;
+            string numberAndSuit = number + suitString;
 
-            return new string[]{
-                $"{blackOnWhite}-----------{resetCol}",
-                $"{blackOnWhite}| {suitAndNumber}      |{resetCol}",
-                $"{blackOnWhite}|         |{resetCol}",
-                $"{blackOnWhite}|         |{resetCol}",
-                $"{blackOnWhite}|         |{resetCol}",
-                $"{blackOnWhite}|         |{resetCol}",
-                $"{blackOnWhite}|         |{resetCol}",
-                $"{blackOnWhite}-----------{resetCol}"
+            string[] cardString = new string[]{
+                $"{blackOnWhite}-----------------{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}|               |{resetCol}",
+                $"{blackOnWhite}-----------------{resetCol}",
+                ""
                 };
+
+            cardString[1] = cardString[1].Remove(10,2).Insert(10, suitAndNumber);
+            cardString[9] = cardString[9].Remove(21,2).Insert(21, numberAndSuit);
+
+            switch (value)
+            {
+                case 1:
+                    cardString[5] = cardString[5].Remove(16,1).Insert(16, suitString);
+                break;
+
+                case 2:
+                    cardString[3] = cardString[3].Remove(16,1).Insert(16, suitString);
+                    cardString[7] = cardString[7].Remove(16,1).Insert(16, suitString);
+                break;
+
+                case 3:
+                    cardString[3] = cardString[3].Remove(16,1).Insert(16, suitString);
+                    cardString[5] = cardString[5].Remove(16,1).Insert(16, suitString);
+                    cardString[7] = cardString[7].Remove(16,1).Insert(16, suitString);
+                break;
+
+                case 4:
+                    cardString[3] = cardString[3].Remove(19,1).Insert(19, suitString);
+                    cardString[7] = cardString[7].Remove(19,1).Insert(19, suitString);
+                    cardString[3] = cardString[3].Remove(13,1).Insert(13, suitString);
+                    cardString[7] = cardString[7].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                case 5:
+                    cardString[5] = cardString[5].Remove(16,1).Insert(16, suitString);
+                    cardString[3] = cardString[3].Remove(19,1).Insert(19, suitString);
+                    cardString[7] = cardString[7].Remove(19,1).Insert(19, suitString);
+                    cardString[3] = cardString[3].Remove(13,1).Insert(13, suitString);
+                    cardString[7] = cardString[7].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                case 6:
+                    cardString[3] = cardString[3].Remove(19,1).Insert(19, suitString);
+                    cardString[5] = cardString[5].Remove(19,1).Insert(19, suitString);
+                    cardString[7] = cardString[7].Remove(19,1).Insert(19, suitString);
+                    cardString[3] = cardString[3].Remove(13,1).Insert(13, suitString);
+                    cardString[5] = cardString[5].Remove(13,1).Insert(13, suitString);
+                    cardString[7] = cardString[7].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                case 7:
+                    cardString[4] = cardString[5].Remove(16,1).Insert(16, suitString);
+                    cardString[3] = cardString[3].Remove(19,1).Insert(19, suitString);
+                    cardString[5] = cardString[5].Remove(19,1).Insert(19, suitString);
+                    cardString[7] = cardString[7].Remove(19,1).Insert(19, suitString);
+                    cardString[3] = cardString[3].Remove(13,1).Insert(13, suitString);
+                    cardString[5] = cardString[5].Remove(13,1).Insert(13, suitString);
+                    cardString[7] = cardString[7].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                case 8:
+                    cardString[4] = cardString[5].Remove(16,1).Insert(16, suitString);
+                    cardString[6] = cardString[6].Remove(16,1).Insert(16, suitString);
+                    cardString[3] = cardString[3].Remove(19,1).Insert(19, suitString);
+                    cardString[5] = cardString[5].Remove(19,1).Insert(19, suitString);
+                    cardString[7] = cardString[7].Remove(19,1).Insert(19, suitString);
+                    cardString[3] = cardString[3].Remove(13,1).Insert(13, suitString);
+                    cardString[5] = cardString[5].Remove(13,1).Insert(13, suitString);
+                    cardString[7] = cardString[7].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                case 9:
+                    cardString[5] = cardString[5].Remove(16,1).Insert(16, suitString);
+                    cardString[2] = cardString[2].Remove(19,1).Insert(19, suitString);
+                    cardString[4] = cardString[4].Remove(19,1).Insert(19, suitString);
+                    cardString[6] = cardString[6].Remove(19,1).Insert(19, suitString);
+                    cardString[8] = cardString[8].Remove(19,1).Insert(19, suitString);
+                    cardString[2] = cardString[2].Remove(13,1).Insert(13, suitString);
+                    cardString[4] = cardString[4].Remove(13,1).Insert(13, suitString);
+                    cardString[6] = cardString[6].Remove(13,1).Insert(13, suitString);
+                    cardString[8] = cardString[8].Remove(13,1).Insert(13, suitString);
+                break;
+                
+                default:
+                break;
+            }
+
+            return cardString;
         }
     }   
 }
